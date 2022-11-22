7822f322874bb043373a4f3caf8d919e20c1b4ed
kiwi-fruitiwi <zane.tian@gmail.com>
2022-11-22 12:47:56 -0800

    add syntaxAnalyzer testing plan+strategy comments

--- syntaxAnalyzer.py
+++ syntaxAnalyzer.py
@@ -1,77 +1,35 @@
 """
 @author kiwi
-@date 2022.08.14
+@date 2022.11.26
 
-project contract
-  implement syntaxAnalyzer in python
-    parses .jack files → xml from nand2tetris/projects/10
-    compare with TextComparer in nand2tetris/tools
-  see chapter.10 of book
-
-  1. build JackTokenizer ← unit 4.8
-  2. build CompilationEngine, basic expressionless → complete with expressions
-    xml file needs to be wrapped in <tokens> tag
-    string constants have no double quotes
-    remember html special characters [< > " &] → [&lt; &gt; &quot; &amp]
-
-  currentToken varName has 5 branches:
-    foo
-    foo[expression]
-    foo.bar(expList)
-    Foo.bar(expList) ← what's the difference? static method vs member?
-    bar(expList)
-
-  this is the only case where the jack grammar is LL(2)
-    save current token
-    look ahead to next one
-
-
-  ⊼² finish tokenizer, 📹→L4.8, 12:57 is compilation engine
-    test using *T.xml files after reading *.jack
-    ExpressionLessSquare after basic compilationEngine done
-    Square when expressions done
-
-→ a bunch of characters with no space in the middle does not necessarily mean that you have only one token, e.g. getx() has three: getx and two symbols
-    look up getx in keywords list. not there, so must be identifier
-    next is straightforward symbol table lookup
-
-  consider implementations in library string functions and regex
-    check lexical elements diagram in this order. nc = next character
-        check if nc is in the symbols list
-        integerConstant → is nc [0-9]? take rest of [0-9]
-        surrounded by "" → stringConstant. next "[all char ex. " \n]"
-        else nc is alphabetical:
-            check keywords list for next token. should be all alphabetic
-            identifier → [0-9a-zA-Z_]+
-
-  tokenizing strategy
-    observations:
-        every line ends with a newline or symbol
-        every token ends with a space or a symbol (or newline)
-        → find position of next space, \n, or symbol →  evaluate current token
-
-    advance()
-    for code line `class Main {`
-        first character 'c'
-        append until we reach: ⎵ \n symbol → 'class'
-            identify as keyword. if it hadn't, identifier
-        for 'Main', append until ⎵. same as above
-
-    for code line 'function void main() {'
-        main() will parse 'main' and arrive upon a symbol
-
-    for code line `let length = Keyboard.readInt("HOW MANY NUMBERS? ");`
-        reading in the first double quote makes us find the next one in the line
-            throw error if not
-
-    for code line `let i = 0;`
-        if numeric, must be an integerConstant because identifiers can't start
-            eat until delimiter: ⎵ \n symbol
-
-    method: #detectDelimiter returns true if next char is ⎵, \n, symbol
-
-  tokenizer testing method: line by line parsing: one new category at a time
-  tokenizer pseudocode
+test programs for evolving compiler
+	seven
+		arithmetic expression involving constants only
+		do + return statements
+	convertToBin
+		arbitrarily choose output location
+		converts RAM[8000] to binary → 16 bits in RAM[8001-8016]
+		tests:
+			expressions without arrays or method calls
+			procedural constructs: if while do let return
+		tips for testing the compiled code
+			cannot access RAM in 'no animation' mode
+			use binoculars to look at address 8000
+			click 'stop' button to see the results in state of the RAM
+	square: constructors, methods, expression including method calls
+		multiple files! Square, SquareGame, Main
+	average: arrays and strings
+	pong: complete object-oriented app with objects, static vars
+		compile Bat, PongGame, Main, Ball
+		delay execution. reduce speed slider to play game
+	complexArrays: handles array manipulation with fancy indices
+		a[b[a[3]]] = a[a[5]] * b[7-a[3]-Main.double(2)+1];
+		test easily via screen's Output
+use compiler to compile the program directory
+inspect generated code
+if no errors, load directory into VM emulator
+run compiled program → inspect results
+if problem, fix compiler and repeat
 
 """
 
@@ -82,12 +40,9 @@ from compilationEngine import CompilationEngine
 from tokenizer import JackTokenizer
 from tokenizer import TokenType
 
-from pathlib import Path
-import os
-
 
 def generateTokensFromJack():
-    root: str = 'C:/Dropbox/code/nand2tetris/kiwi/nand2tetris/projects/'
+    # root: str = 'C:/Dropbox/code/nand2tetris/kiwi/nand2tetris/projects/'
     # filename: str = root + '10/ArrayTest/Main.jack'
     filename: str = 'test.jack'
 
@@ -123,13 +78,15 @@ def generateTokensFromJack():
         output.write(f'<{tagName}> {value} </{tagName}>\n')
         tk.advance()
 
+
     output.write(f'</tokens>\n')
+
 
 # generateTokensFromJack()
 
 
 def generateCompilationEngineOutput():
-    root: str = 'C:/Dropbox/code/nand2tetris/kiwi/nand2tetris/projects/'
+    # root: str = 'C:/Dropbox/code/nand2tetris/kiwi/nand2tetris/projects/'
     # filename: str = root + '10/ArrayTest/Main.jack'
     filename: str = 'test.jack'
     outputUri = 'output.xml'  # compilation engine output
