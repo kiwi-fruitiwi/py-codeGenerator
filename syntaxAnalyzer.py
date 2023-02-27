11050caf25e0940c9aa026d46d2b3bea310a7cc6
kiwi <zane.tian@gmail.com>
2023-02-26 20:13:55 -0800

    add syntaxAnalyzer.main: compile single file with paths

--- syntaxAnalyzer.py
+++ syntaxAnalyzer.py
@@ -39,9 +39,12 @@ if problem, fix compiler and repeat
 # take care of multiple files in a directory vs one target file
 
 from compilationEngine import CompilationEngine
-
 from tokenizer import JackTokenizer
 from tokenizer import TokenType
+
+# used for Path().stem
+from pathlib import Path
+import os
 
 
 def generateTokensFromJack():
@@ -88,15 +91,119 @@ def generateTokensFromJack():
 # generateTokensFromJack()
 
 
-def generateCompilationEngineOutput():
-    # root: str = 'C:/Dropbox/code/nand2tetris/kiwi/nand2tetris/projects/'
+def compileSingleFile(filename: str, xmlOutput: str, vmOutput: str):  # format: 'test.jack'
+    # root: str = 'C:/Users/kiwi/Dropbox/code/nand2tetris/kiwi/nand2tetris/projects/'
     # filename: str = root + '10/ArrayTest/Main.jack'
-    filename: str = 'test.jack'
-    xmlOutput = 'output.xml'  # syntax analyzer output: tokens
-    VMOutput = 'output.vm'  # VMWriter output: VM code
 
-    ce = CompilationEngine(filename, xmlOutput, VMOutput)
+    ce = CompilationEngine(filename, xmlOutput, vmOutput)
     ce.testCompile()
 
 
-generateCompilationEngineOutput()
+def main(path: str) -> None:
+    # main must determine if filename is directory or file
+    # → and instantiate parser objects to read .vm files inside the directory
+
+    """
+    encapsulate parser writer loop with a single method
+    save the directory name
+
+        if a file is detected:
+            parser reads loc.vm
+            writer outputs to loc.asm
+        if directory:
+            vmFileList ← generate
+                parser reads all .vm files in vmFileList
+                while codeWriter writes each one to loc.asm
+
+    :param path:
+    :return:
+    """
+
+    # os.path.abspath returns abspath from where the .py file is executing
+    if os.path.isfile(path):
+        print(f'\n🐳 file detected: {path}')
+        # run parser writer loop on the file
+        # todo find directory name. or chop off .vm and replace with .asm
+
+        basename = os.path.basename(os.path.dirname(path))
+        print(f'directory name → {basename}')
+
+        stem = Path(path).stem  # a stem is a filename without an extension
+        print(f'stem → {stem}')
+
+        path = Path(path)
+        parentPath = path.parent.absolute()
+        print(f'parent path → {str(parentPath) + os.sep}')
+
+        # if the path is a file, set .xml and .vm output paths
+        xmlOutputPath = str(parentPath) + os.sep + stem + ".xml"
+        vmOutputPath = str(parentPath) + os.sep + stem + ".vm"
+        readpath = str(path)
+        basename = os.path.basename(os.path.dirname(path))
+
+        print(f'readpath → {readpath}')
+        print(f'basename → {basename}\n\n')
+        compileSingleFile(path, xmlOutputPath, vmOutputPath)
+
+    elif os.path.isdir(path):
+        print(f'[DETECT] directory detected: {path}')
+        # if the path is a directory, generate list of vm files in directory
+        # run parser writer loop on each one; codeWriter uses 'w[rite]' mode at
+        # first, then '[a]ppend' mode for subsequent files in the list
+
+        # loop through .vm files in directory
+        for file in os.listdir(path):
+            if file.lower().endswith('.vm'):
+                print(f'🚙 looping through vm files → {file}, '
+                      f'{os.path.abspath(file)}')
+        print('\n')
+
+        # detect .vm files in this directory
+        # save directory root, which always contains a slash at the end?
+        root = path
+        print(f'root → {root}')
+
+        # basename is the name of the directory, e.g.
+        # c:/Dropbox/StaticTest/ → StaticTest
+        basename = os.path.basename(os.path.dirname(path))
+        print(f'os.path.dirname(absPath) → {os.path.dirname(path)}')
+        print(f'dirname → {basename}\n')
+
+        outputPath = root + basename + ".asm"
+        print(f'✒ overwriting {outputPath}')
+
+        writer = CodeWriter(outputPath)
+        writer.writeBootstrap()
+
+        '''
+        overwrite .asm output if this is the first time we're in a directory,
+        but append for all following files ← deprecated because we now open 
+        codeWriter only once.
+
+        we must start with Sys.vm, and then the other files; raise an error 
+        if Sys.vm is not present, but move it to index 0 if it is
+        '''
+        filesInDirectory = os.listdir(path)
+        if 'Sys.vm' not in filesInDirectory:
+            raise ValueError(f'Sys.vm not present in directory.')
+        else:
+            vmFiles = []
+            for file in filesInDirectory:
+                if file.lower().endswith('.vm'):
+                    if file != 'Sys.vm':
+                        vmFiles.append(file)
+            vmFiles.insert(0, 'Sys.vm')
+
+        for file in vmFiles:
+            if file.lower().endswith('.vm'):  # 'file' is a VM file
+                readPath = root + file
+                print(f'📃 translating: {readPath}')
+
+                # TODO: file should strip .vm :p
+                translate(readPath, writer, file)
+
+    else:
+        raise ValueError(f'{path} does not seem to be a file or directory')
+
+
+main('tests/Seven/Main.jack')
